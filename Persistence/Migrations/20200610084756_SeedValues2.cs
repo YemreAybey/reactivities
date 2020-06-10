a963228bcfe9fa42c7ccdd518d6c2107a1ebdcfe
ext-s.gurkan <serhat.gurkan@pabeda.com.tr>
2020-06-10 13:49:44 +0300

    initial commit

---
+++ Persistence/Migrations/20200610084756_SeedValues2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class SeedValues2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Value 102");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Value 103");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Value 101");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Value 101");
+        }
+    }
+}
